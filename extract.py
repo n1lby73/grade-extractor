48452d813152e2ce0d43d116c462cb2a89d5176e
n1lby73 <n1lby73@gmail.com>
2023-07-21 16:34:35 +0100

    all data can be extracted and generate new file

--- extract.py
+++ extract.py
@@ -1,14 +1,42 @@
 import openpyxl
 import pandas as pd
 
-template = openpyxl.load_workbook('results.xlsx')
+template = openpyxl.load_workbook('template.xlsx')
+worksheet = template.worksheets[0]
 
 data = pd.read_excel('results.xlsx')
 
+scoreList = []
+currentCourseColumn = 11
+
 for index, row in data.iterrows():
+
     lastName = row['SURNAME']
     firstName = row['FIRST NAME']
     middleName = row['MIDDLE NAME']
     
+    score = data.iloc[index,5:].values
+
+    score = pd.Series(score).fillna('NA').values
+
+    for scores in score[:33]:
+
+        scoreList.append(scores)
+    
+    average = scoreList[32]
+
+    for scores in scoreList[:32]:
+
+        worksheet.cell(row=currentCourseColumn, column=4, value=scores)
+
+        currentCourseColumn += 1
+
     name = lastName +" " + firstName + " " + middleName
-    print (name)
+
+    worksheet.cell(row=6, column=3, value=name)
+    worksheet.cell(row=6, column=5, value=average)
+
+    scoreList = []
+    currentCourseColumn = 11
+
+    template.save('{}.xlsx'.format(name))
