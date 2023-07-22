3cc598167ebc84abd8b34c711b5b337bb921eca3
n1lby73 <n1lby73@gmail.com>
2023-07-22 09:09:24 +0100

    generating class details with user intput

--- extract.py
+++ extract.py
@@ -1,42 +1,102 @@
 import openpyxl
 import pandas as pd
+import os
+import sys
 
-template = openpyxl.load_workbook('template.xlsx')
+print ("1. EMY \n2. ETY")
+
+try:
+
+    programe = int(input("Kindly input which programe to generate result for: "))
+
+    if programe != 1 and programe != 2:
+
+        print('wrong value entered')
+
+        os.execv(sys.executable, ['python'] + sys.argv)
+    
+    if programe == 1:
+
+        programe = 'EMY'
+
+    else:
+
+        programe = "ETY"
+
+except ValueError:
+
+    print('Please select from the above option')
+
+    os.execv(sys.executable, ['python'] + sys.argv)
+
+while True:
+
+    try:
+
+        classNumber = int(input("Please input only number excluding 'C': "))
+
+        className = programe+"-C"+str(classNumber)
+
+        workbook = openpyxl.load_workbook('emyety.xlsm')
+        sheet_names = workbook.sheetnames
+        
+        if className not in sheet_names:
+
+            print("Class details not available in excel database\nKindly provide all necessary details again or update excel database")
+
+            os.execv(sys.executable, ['python'] + sys.argv)
+
+        break
+
+    except ValueError:
+
+        print("Please input a number\n")
+
+
+className = programe+"-C"+classNumber
+
+workbook = openpyxl.load_workbook('emyety.xlsm')
+sheet_names = workbook.sheetnames
+
+template = openpyxl.load_workbook('emyTemplate.xlsx')
 worksheet = template.worksheets[0]
 
-data = pd.read_excel('results.xlsx', skiprows=1, header=None)
+# workbook = openpyxl.load_workbook('emyety.xlsm')
+# sheet_names = workbook.sheetnames
 
-scoreList = []
-currentCourseColumn = 11
+# data = pd.read_excel('results.xlsx', skiprows=1, header=None)
 
-for _, row in data.iterrows():
+# scoreList = []
+# currentCourseColumn = 11
 
-    lastName = row.iloc[1]
-    firstName = row.iloc[1]
-    middleName = row.iloc[2]
+# for _, row in data.iterrows():
+
+#     lastName = row.iloc[1]
+#     firstName = row.iloc[1]
+#     middleName = row.iloc[2]
     
-    score = row.iloc[5:].values
+#     score = row.iloc[5:].values
 
-    score = pd.Series(score).fillna('NA').values
+#     score = pd.Series(score).fillna('NA').values
 
-    for scores in score[:33]:
+#     for scores in score[:33]:
 
-        scoreList.append(scores)
+#         scoreList.append(scores)
     
-    average = scoreList[32]
+#     average = scoreList[32]
 
-    for scores in scoreList[:32]:
+#     for scores in scoreList[:32]:
 
-        worksheet.cell(row=currentCourseColumn, column=4, value=scores)
+#         worksheet.cell(row=currentCourseColumn, column=4, value=scores)
 
-        currentCourseColumn += 1
+#         currentCourseColumn += 1
 
-    name = lastName +" " + firstName + " " + middleName
+#     name = lastName +" " + firstName + " " + middleName
 
-    worksheet.cell(row=6, column=3, value=name)
-    worksheet.cell(row=6, column=5, value=average)
+#     worksheet.cell(row=6, column=3, value=name)
+#     worksheet.cell(row=6, column=5, value=average)
 
-    scoreList = []
-    currentCourseColumn = 11
+#     scoreList = []
+#     currentCourseColumn = 11
 
-    template.save('{}.xlsx'.format(name))
+#     template.save('{}.xlsx'.format(name))
