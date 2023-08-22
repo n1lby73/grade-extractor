6431ae24b2f73be8eda5babe0154264f344d0158
n1lby73 <n1lby73@gmail.com>
2023-08-22 05:50:48 +0100

    added flask restful and jwt modules

--- webApp/gradetractor/__init__.py
+++ webApp/gradetractor/__init__.py
@@ -1,10 +1,14 @@
+from flask_jwt_extended import JWTManager
 from flask_pymongo import PyMongo
+from flask_restful import Api
 from flask import Flask
 
 app = Flask(__name__)
 
 from gradetractor import config
 
+api = Api(app)
 mongo = PyMongo(app)
+jwt = JWTManager(app)
 
 from gradetractor import apiroutes
