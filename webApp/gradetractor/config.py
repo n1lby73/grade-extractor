a660202b2e4aed5213ae8e83da92884150e0ef70
n1lby73 <n1lby73@gmail.com>
2023-08-28 00:06:01 +0100

    added new config

--- webApp/gradetractor/config.py
+++ webApp/gradetractor/config.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+# app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
-app.config['ALLOWED_EXTENSIONS'] = os.getenv('ALLOWED_EXTENSIONS')
+app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
+app.config['ALLOWED_EXTENSIONS'] = os.getenv('ALLOWED_EXTENSION')
