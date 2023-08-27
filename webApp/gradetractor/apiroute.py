999fb79a59a91e10971b4d792a9fc32ce80f010f
n1lby73 <n1lby73@gmail.com>
2023-08-27 19:16:57 +0100

    route to upload excel result db

--- webApp/gradetractor/apiroute.py
+++ webApp/gradetractor/apiroute.py
@@ -1,52 +1,64 @@
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, decode_token, jwt_manager
 from flask_restful import Resource, reqparse
 from flask import jsonify, request, session
-from gradetractor import api, jwt, db
+from gradetractor import api, jwt
 import openpyxl
 
 # resultSheet = "emyety.xlsx"
 
 # Custom validation function to check the file extension
 def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    ALLOWED_EXTENSIONS = ['.xlsx']
+    
+    if "." in filename:
+        
+        # Perform string formatting to retrieve only extension
+
+        startIndex = filename.find(".")
+        endIndex = filename.find("'", startIndex)
+      
+        extension = filename[startIndex:endIndex]
+
+        if extension in ALLOWED_EXTENSIONS:
+            
+            # string formatting to extract file name 
+
+            nameIndexStart = filename.find("'")
+            nameIndexEnd = filename.find(".", nameIndexStart+1) # +1 to omit " ' "
+
+            name = filename[nameIndexStart+1:nameIndexEnd]+extension
+            # print (name)
+            return name
 
 class results(Resource):
-    @jwt_required()
+    # @jwt_required()
     def __init__(self):
 
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('file', type='FileStorage', location='files', required=True, dest='files', validation=allowed_file)
+        self.parser.add_argument('file',  required=True, type='FileStorage', location='files',  dest='files')
         # self.parser.add_argument("resultdb", required=True)
 
-    def get(self):
+    def post(self):
+        
+        args = self.parser.parse_args()
+        uploaded_result = args['files']
 
-        resultDb = openpyxl.load_workbook(resultSheet)
-        classes = resultDb.sheetnames
-        print (classes)
+        checkFileFormat = allowed_file(uploaded_result)
+        print (checkFileFormat)
+        if checkFileFormat:
 
-        filteredClasses = []
-        unwantedWorkbook = []
-
-        for i in classes:
-
-            if "emy" in i.lower() or "ety" in i.lower():
-
-                filteredClasses.append(i)
-
-        for i in filteredClasses:
-
-            if "Assessment" in i or ">" in i:
-                unwantedWorkbook.append(i)
-
-        filteredClasses = [classes for classes in filteredClasses if classes not in unwantedWorkbook]
-
-        return jsonify(allClasses=filteredClasses)
+            session['excelResultDb'] = checkFileFormat  # Store the files in the session
+            
+            return {'message': 'uploaded successfully'}, 200
+        
+        return {'error': "wrong filetype"}, 406
 
 class allClasses(Resource):
     @jwt_required()
     def get(self):
 
+        resultSheet = session.get('excelResultDb')
         resultDb = openpyxl.load_workbook(resultSheet)
         classes = resultDb.sheetnames
         print (classes)
@@ -113,9 +125,10 @@ class reg(Resource):
 
         # users_collection = db.usersfhg
         # users_collection.insert({'email':email, 'password':password})
-        db.users.insert_one({'email':email, 'password':password})
+        # db.users.insert_one({'email':email, 'password':password})
 
 api.add_resource(reg, '/api/v1/reg', '/api/v1/reg/')
 api.add_resource(login, '/api/v1/login', '/api/v1/login/')
+api.add_resource(results, '/api/v1/result', '/api/v1/result/')
 api.add_resource(allClasses, '/api/v1/index', '/api/v1/index/')
 api.add_resource(genResult, '/api/v1/genResult', '/api/v1/genResult/')
