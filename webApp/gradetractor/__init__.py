a66c4dbb8a987c42e9ad902b68a5c194e9f50957
n1lby73 <n1lby73@gmail.com>
2024-09-30 21:39:24 +0100

    settling cors

--- webApp/gradetractor/__init__.py
+++ webApp/gradetractor/__init__.py
@@ -2,15 +2,19 @@ from flask_jwt_extended import JWTManager
 from flask_pymongo import PyMongo
 from flask_restful import Api
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.app_context().push()
 
 from gradetractor import config
 
 api = Api(app)
 mongo = PyMongo(app)
+# db = mongodb.db
 jwt = JWTManager(app)
+CORS(app, cors_allowed_origin=['http://127.0.0.1:5000/api/v1/result'])
 
-users_collection = mongo.db.users
+# users_collection = mongo.db.users
 
 from gradetractor import apiroute
